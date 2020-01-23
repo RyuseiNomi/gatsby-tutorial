8040c56b22cfdf146162692ccc4b096bd25de232
RyuseiNomi <kanontonto1010@gmail.com>
2020-01-23 09:57:27 +0900

    use Link tag and create a new page named contact

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default () => (
   <div style={{color:`purple`}}>
-    <h1>Hello world!</h1>
+    <Link to="/contact/">Contact</Link>
+    <Header headerText="Hello Gatsby!" />
     <p>What a world.</p>
     <img src="https://avatars2.githubusercontent.com/u/25520340?s=460&v=4" alt="" />
   </div>
