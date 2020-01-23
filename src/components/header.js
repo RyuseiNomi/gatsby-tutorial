41180095a8b3d3256fd0aa145928b307d3c4d247
RyuseiNomi <kanontonto1010@gmail.com>
2020-01-23 09:50:12 +0900

    pass a few value to sub-component

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,8 @@
 import React from "react"
 
-export default props => <h1>{props.headerText}</h1>
+export default props => (
+  <div style={{color:`purple`}}>
+    <h1>{props.headerText}</h1>
+    <h1>{props.arbitraryPhrase}</h1>
+  </div>
+)
--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import Header from "../components/header.js"
 
 export default () => (
   <div style={{ color: `teal` }}>
-    <Header headerText="About Gatsby"/>
+    <Header headerText="About Gatsby" arbitraryPhrase="is arbitrary"/>
     <p>Such wow. Very React.</p>
   </div>
 )
