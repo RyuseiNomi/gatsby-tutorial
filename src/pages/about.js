aaf5183cde6f461c5d308e4ba968f6f17d0555c0
RyuseiNomi <kanontonto1010@gmail.com>
2020-01-23 09:33:22 +0900

    add header sub-components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,9 @@
 import React from "react"
+import Header from "../components/header.js"
 
 export default () => (
   <div style={{ color: `teal` }}>
+    <Header />
     <h1>About Gatsby</h1>
     <p>Such wow. Very React.</p>
   </div>
