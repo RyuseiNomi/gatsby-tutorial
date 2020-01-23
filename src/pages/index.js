11fbf401adeae51508b7dbaa4171425f1e491850
RyuseiNomi <kanontonto1010@gmail.com>
2020-01-23 09:24:42 +0900

    update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div style={{color:`purple`}}>
+    <h1>Hello world!</h1>
+    <p>What a world.</p>
+    <img src="https://avatars2.githubusercontent.com/u/25520340?s=460&v=4" alt="" />
+  </div>
+)
